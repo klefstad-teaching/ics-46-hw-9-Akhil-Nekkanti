977b7964d6b58bfd43c844932b45321ba2be9ec1
Akhil-Nekkanti <akhil.nekkanti@gmail.com>
2025-03-15 22:56:56 +0000

    ladder?

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -23,4 +23,40 @@ bool edit_distance_within(const string& str1, const string& str2, int d) {
     return (differences + (len1 - i) + (len2 - j)) <= d;
 }
 
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    if (begin_word == end_word) {
+        error(begin_word, end_word, "Start and end words must be different");
+        return {};
+    }
+    
+    queue<vector<string>> ladder_queue;
+    set<string> visited;
+    
+    ladder_queue.push({begin_word});
+    visited.insert(begin_word);
+    
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        
+        string last_word = ladder.back();
+        
+        for (const string& word : word_list) {
+            if (is_adjacent(last_word, word) && visited.find(word) == visited.end()) {
+                vector<string> new_ladder = ladder;
+                new_ladder.push_back(word);
+                visited.insert(word);
+                
+                if (word == end_word) return new_ladder;
+                
+                ladder_queue.push(new_ladder);
+            }
+        }
+    }
+    return {};
+}
 
