0d3258586a98a0ee84152c3d5a5b92b41d325948
Akhil-Nekkanti <akhil.nekkanti@gmail.com>
2025-03-16 00:11:57 +0000

    ladder done?

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -83,3 +83,17 @@ void print_word_ladder(const vector<string>& ladder) {
         cout << endl;
     }
 }
+
+void verify_word_ladder() {
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+    
+    string start_word, end_word;
+    cout << "Enter start word: ";
+    cin >> start_word;
+    cout << "Enter end word: ";
+    cin >> end_word;
+    
+    vector<string> ladder = generate_word_ladder(start_word, end_word, word_list);
+    print_word_ladder(ladder);
+}
